5311d6cf12dac6ecac7b7ccbbe982abaf246f786
Deniss Melniks <denmel@inbox.lv>
2021-04-29 14:02:35 +0300

    before mocito

--- app/src/test/java/lv/lu/finalwork/service/ProductServiceTest.java
+++ app/src/test/java/lv/lu/finalwork/service/ProductServiceTest.java
@@ -14,7 +14,7 @@ public class ProductServiceTest {
 
     @Before
     public void setUp() throws Exception {
-        service = new ProductService();
+ //       service = new ProductService();
     }
 
     @Test
